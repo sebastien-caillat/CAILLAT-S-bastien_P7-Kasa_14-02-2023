8535735700f5a2e117440ef1f12abd44cf783db7
Sébastien Caillat <sebastien.caillat96@gmail.com>
2023-02-18 19:15:20 +0100

    Card v2
    Cards generated and visible, need to add CSS

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,6 +4,9 @@ import housing from '../data/logements.json'
 
 const CardGallery = styled.div`
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding-top: 50px;
     margin-top: 450px;
     margin-bottom: -800px;
     width: 90%;
@@ -15,6 +18,16 @@ const CardItem = styled.div`
     padding: 20px;
     width: 340px;
     height: 340px;
+    border: 2px solid black;
+    border-radius: 10px;
+`
+const CardImg = styled.img`
+    width: 100%;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+`
+const CardTitle = styled.h2`
+    font-size: 18px;
 `
 const housingCard = housing;
 
@@ -22,7 +35,10 @@ function Card() {
     return(
         <CardGallery>
             {housingCard.map(({ id, title, cover }) => (
-                <CardItem key={id} title={title} cover={cover} />
+                <CardItem key={id} cover={cover} title={title}>
+                    <CardImg src={cover} alt='' />
+                    <CardTitle>{title}</CardTitle>
+                </CardItem>                
             ))}
         </CardGallery>
     )
